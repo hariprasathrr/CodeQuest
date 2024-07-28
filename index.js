2455b6d663936e3c4cebab49e706a52ff9fc1f20
Hariprasath <136950381+hariprasathrr@users.noreply.github.com>
2024-07-28 18:56:30 +0530

    Leet code groupedAnagrams solution

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-function isAnagram(s, t) {
-  if (s.length !== t.length) {
-    return false;
-  }
+function groupAnagrams(strs) {
+  let grouped = {};
 
-  const sCharCounts = {}; //{H:1,E:1,L:2,O:1}
-
-  //Fill sCharCounts
-  for (let i = 0; i < s.length; i++) {
-    const sChar = s[i];
-    sCharCounts[sChar] = sCharCounts[sChar] + 1 || 1;
-  }
-
-  for (let i = 0; i < t.length; i++) {
-    const tChar = t[i];
-
-    if (!sCharCounts[tChar]) {
-      return false;
-    } else {
-      sCharCounts[tChar]--;
+  for (let i = 0; i < strs.length; i++) {
+    const word = strs[i];
+    const key = word.split("").sort().join("");
+    if (!grouped[key]) {
+      grouped[key] = [];
     }
+    grouped[key].push(word);
   }
-
-  return true;
+  return Object.values(grouped);
 }
 
-module.exports = isAnagram;
+module.exports = groupAnagrams;
