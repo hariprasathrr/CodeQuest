48cb1925a63f001eff3a68d88af04446c35b5c3c
Hariprasath <136950381+hariprasathrr@users.noreply.github.com>
2024-07-29 20:30:39 +0530

    Leet Code ValidParenthesis test code

--- test.js
+++ test.js
@@ -1,18 +1,21 @@
-const groupAnagrams = require("./index");
+const isValid = require("./index");
 
-test("['eat', 'tea', 'tan', 'ate', 'nat', 'bat'] returns 3 groups of Anagrams", () => {
-  let res = groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]).sort(
-    (a, b) => b.length - a.length
-  );
+test("'(', ')', '{', '}', '[', ']' should return false", () => {
+  expect(isValid("(")).toEqual(false);
+  expect(isValid(")")).toEqual(false);
+  expect(isValid("{")).toEqual(false);
+  expect(isValid("}")).toEqual(false);
+  expect(isValid("[")).toEqual(false);
+  expect(isValid("]")).toEqual(false);
+});
 
-  res.forEach(subArr => subArr.sort());
+test("'(]', '([)]' should return false", () => {
+  expect(isValid("(]")).toEqual(false);
+  expect(isValid("([)]")).toEqual(false);
+});
 
-  expect(res.length).toEqual(3);
-
-  const subArr1 = res[0];
-  expect(subArr1).toEqual(["ate", "eat", "tea"]);
-  const subArr2 = res[1];
-  expect(subArr2).toEqual(["nat", "tan"]);
-  const subArr3 = res[2];
-  expect(subArr3).toEqual(["bat"]);
+test("'()', '()[]{}', '{[]}' should return true", () => {
+  expect(isValid("()")).toEqual(true);
+  expect(isValid("()[]{}")).toEqual(true);
+  expect(isValid("{[]}")).toEqual(true);
 });
