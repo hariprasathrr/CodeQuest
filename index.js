59eed092f0a757d8820c2ce8e2e3ccd248c761c2
Hariprasath <136950381+hariprasathrr@users.noreply.github.com>
2024-07-31 20:30:55 +0530

    House Robber leetcode problem solution

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const climbStairs = (n) => {
-  if (n <= 3) return n;
+function rob(nums) {
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return nums[0];
+  if (nums.length === 2) return Math.max(nums[0], nums[1]);
 
-  let ways = [0, 1, 2, 3];
+  let maxLootAtNth = [nums[0], Math.max(nums[0], nums[1])];
 
-  for (let i = 4; i <= n; i++) {
-    ways.push(ways[i - 1] + ways[i - 2]);
+  for (let i = 2; i < nums.length; i++) {
+    maxLootAtNth.push(
+      Math.max(nums[i] + maxLootAtNth[i - 2], maxLootAtNth[i - 1])
+    );
   }
-  return ways[n];
-};
+  return maxLootAtNth.pop();
+}
 
-module.exports = climbStairs;
+module.exports = rob;
