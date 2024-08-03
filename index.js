276f09989252c8a4c80da22678b82d2994402020
Hariprasath <136950381+hariprasathrr@users.noreply.github.com>
2024-08-03 18:51:32 +0530

    Unique paths leet code problem solution

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-function coinChange(coins, amount) {
-  let dpMinCoins = new Array(amount + 1).fill(Infinity);
-  dpMinCoins[0] = 0;
+function uniquePaths(m, n) {
+  const dpMatrix = [];
 
-  for (let i = 1; i < dpMinCoins.length; i++) {
-    for (j = 0; j < coins.length; j++) {
-      const coinValue = coins[j];
-      if (coinValue <= i) {
-        dpMinCoins[i] = Math.min(dpMinCoins[i - coinValue] + 1, dpMinCoins[i]);
-      }
+  for (let row = 1; row <= n; row++) {
+    dpMatrix.push([]);
+  }
+
+  //fill out first row of dp matrix
+  for (let row = 0; row < n; row++) {
+    dpMatrix[row][0] = 1;
+  }
+  //fill out first col of dp matrix
+  for (let col = 0; col < m; col++) {
+    dpMatrix[0][col] = 1;
+  }
+
+  for (let row = 1; row < n; row++) {
+    for (let col = 1; col < m; col++) {
+      dpMatrix[row][col] = dpMatrix[row][col - 1] + dpMatrix[row - 1][col];
     }
   }
-  const answer = dpMinCoins[dpMinCoins.length - 1];
-  return answer === Infinity ? -1 : answer;
+  return dpMatrix[dpMatrix.length - 1][m - 1];
 }
 
-module.exports = coinChange;
+module.exports = uniquePaths;
