aa07420c7f671202e503d8b56fd448091aac745c
Hariprasathrr <hariprasathcreator@gmail.com>
2024-08-14 18:32:45 +0530

    Feat : array intersection better code

---
+++ array-intersection-two.js
@@ -0,0 +1,14 @@
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const intersectionArr = [];
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      intersectionArr.push(num);
+    }
+  }
+
+  return intersectionArr;
+}
+
+module.exports = arrayIntersection;
