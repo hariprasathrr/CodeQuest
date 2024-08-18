9f61ae9ada61904e15c9ec32612573e4e426dfb6
Hariprasathrr <hariprasathcreator@gmail.com>
2024-08-18 19:25:06 +0530

    Feat : Are all characters are unique run file

---
+++ are-all-char-unique-run.js
@@ -0,0 +1,9 @@
+const areAllCharactersUnique = require("./are-all-charUnique");
+
+const result1 = areAllCharactersUnique("abacdefg");
+const result2 = areAllCharactersUnique("abcdefgA");
+const result3 = areAllCharactersUnique("programming");
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
