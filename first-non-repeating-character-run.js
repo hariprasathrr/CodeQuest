e78ae78e063994307f16b0e909029dc863982815
Hariprasathrr <hariprasathcreator@gmail.com>
2024-08-19 11:12:20 +0530

    Feat : first non repeating character run file

---
+++ first-non-repeating-character-run.js
@@ -0,0 +1,7 @@
+const findFirstNonRepeatingCharacter = require("./first-non-repeating-character-usingmap");
+
+const result1 = findFirstNonRepeatingCharacter("programming");
+const result2 = findFirstNonRepeatingCharacter("aabbcddeff");
+
+console.log(result1);
+console.log(result2);
