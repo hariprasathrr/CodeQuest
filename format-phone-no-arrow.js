1efad85fc0d963d6b05222144ae08458b851f399
Hariprasathrr <hariprasathcreator@gmail.com>
2024-08-21 20:23:00 +0530

    Feat : Phone number Formatter code

---
+++ format-phone-no-arrow.js
@@ -0,0 +1,6 @@
+const formatPhoneNumber = (numbers) =>
+  `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers
+    .slice(6)
+    .join("")}`;
+
+module.exports = formatPhoneNumber;
