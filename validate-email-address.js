985a86e8e28586bf845c89cbbe321f18defe0109
Hariprasathrr <hariprasathcreator@gmail.com>
2024-08-24 12:40:39 +0530

    Feat : validate email address trandition way code

---
+++ validate-email-address.js
@@ -0,0 +1,21 @@
+function validateEmail(email) {
+  if (email.indexOf("@") === -1) {
+    return false;
+  }
+
+  const [localPart, domain] = email.split("@");
+
+  if (localPart.length === 0 || domain.length < 3) {
+    return false;
+  }
+
+  const domainExtension = domain.split(".");
+
+  if (domainExtension.length < 2 || domainExtension[1] < 2) {
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = validateEmail;
